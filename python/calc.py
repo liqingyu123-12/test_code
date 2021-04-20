9f52d05d2a12a50faa567906160f2e4d4b829dbc
Dell <772307781@qq.com>
2021-04-20 10:01:33 +0800

    更改

---
+++ python/calc.py
@@ -0,0 +1,12 @@
+
+class Calc:
+
+    def add(self, a:int, b:int):
+        return a + b
+
+    def div(self, a, b):
+        # try:
+        #     return a / b
+        # except:
+        #     return "integer division or modulo by zero"
+        return  a / b
---
+++ test/test_calc.py
@@ -0,0 +1,12 @@
+import unittest
+import sys
+sys.path.append('..')
+from python.calc import Calc
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        self.calc = Calc()
+        result = self.calc.add(1,3)
+        print(result)
+        self.assertEqual(4,result)
+
